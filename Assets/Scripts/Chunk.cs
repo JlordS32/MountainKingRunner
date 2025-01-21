4d01cad7d2feac8f9b1ee6afec278ee528d62250
JlordS32 <jlordsty128@gmail.com>
2025-01-21 12:02:36 +1100

    more pickup item

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,22 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Chunk : MonoBehaviour
 {
+    [Header("Prefabs")]
     [SerializeField] private GameObject _fencePrefab;
     [SerializeField] private GameObject _applePrefab;
+    [SerializeField] private GameObject _coinPrefab;
+
+    [Header("Lanes")]
     [SerializeField] private float[] _lanes = { -2f, 1.35f, 4.5f };
-    [SerializeField] private float[] _appleLanes = { -2.5f, 0f, 2.5f };
+    [SerializeField] private float[] _pickupLanes = { -2.5f, 0f, 2.5f };
+
+    [Header("Values")]
+    [SerializeField] private float _appleSpawnChance = 0.335f;
+    [SerializeField] private float _coinSpawnChance = 0.5f;
+    [SerializeField] private int _maxNumbersOfCoinsInLane = 5;
 
     private List<int> availLanes = new List<int> { 0, 1, 2 };
 
     private void Start()
     {
-        SpawnFence();
+        SpawnFences();
         SpawnApple();
+        SpawnCoins();
     }
 
-    private void SpawnFence()
+    private void SpawnFences()
     {
         int fencesToSpawn = Random.Range(0, _lanes.Length);
 
@@ -33,10 +44,26 @@ public class Chunk : MonoBehaviour
 
     private void SpawnApple()
     {
+        if (Random.value > _appleSpawnChance || availLanes.Count == 0) return;
+
         int selectedLane = SelectLane();
 
-        Vector3 spawnPos = new(_appleLanes[selectedLane], transform.position.y, transform.position.z + 5f);
+        Vector3 spawnPos = new(_pickupLanes[selectedLane], transform.position.y, transform.position.z + 5f);
         Instantiate(_applePrefab, spawnPos, Quaternion.identity, this.transform);
+    }
+
+    private void SpawnCoins()
+    {
+        if (Random.value > _coinSpawnChance || availLanes.Count == 0) return;
+
+        int selectedLane = SelectLane();
+
+        int numberOfCoins = Random.Range(1, _maxNumbersOfCoinsInLane);
+        for (int i = 0; i <= numberOfCoins; i++)
+        {
+            Vector3 spawnPos = new(_pickupLanes[selectedLane], transform.position.y, transform.position.z + 5f + -i);
+            Instantiate(_coinPrefab, spawnPos, Quaternion.identity, this.transform);
+        }
     }
 
     private int SelectLane()
