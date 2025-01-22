a646eaaa913e2185c4eb95095b83a4bd519b1f59
JlordS32 <jlordsty128@gmail.com>
2025-01-23 09:22:12 +1100

    adjustments with object spawner

--- Assets/Scripts/ProcGen/Object Spawner.cs
+++ Assets/Scripts/ProcGen/Object Spawner.cs
@@ -7,6 +7,8 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField] private Transform _obstacleParent;
     [SerializeField] private float _delaySeconds = 1f;
     [SerializeField] private float _spawnWidth = 4f;
+    [SerializeField] private int _numToSpawn = 2;
+    [SerializeField] private bool _enableRandomNoSpawn = true;
 
     private void Start()
     {
@@ -15,12 +17,22 @@ public class ObjectSpawner : MonoBehaviour
 
     private IEnumerator SpawnObstacle()
     {
-        while (true) {
-            GameObject obstaclePrefab = _obstaclePrefabs[Random.Range(0, _obstaclePrefabs.Length)];
-            Vector3 spawnPos = new(transform.position.x + Random.Range(-_spawnWidth, _spawnWidth), transform.position.y, transform.position.z);
+        while (true)
+        {
+            int numToSpawn = 1;
+            if (_enableRandomNoSpawn)
+            {
+                numToSpawn = Random.Range(0, _numToSpawn + 1);
+            }
+
+            for (int i = 0; i < numToSpawn; i++)
+            {
+                GameObject obstaclePrefab = _obstaclePrefabs[Random.Range(0, _obstaclePrefabs.Length)];
+                Vector3 spawnPos = new(transform.position.x + Random.Range(-_spawnWidth, _spawnWidth), transform.position.y, transform.position.z);
+                Instantiate(obstaclePrefab, spawnPos, Random.rotation, _obstacleParent);
+            }
 
             yield return new WaitForSeconds(_delaySeconds);
-            Instantiate(obstaclePrefab, spawnPos, Random.rotation, _obstacleParent);
         }
     }
 }
